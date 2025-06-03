1828a7f31d0f28d3c071f23c5a364d5f2d06e381
Elias Orihuela <elias.orihuela@gmail.com>
2025-06-03 20:03:05 -0300

    usuarios

--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Roles(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
 class Usuarios(models.Model):
-    nombre = models.CharField(max_length=100)
-    apellido = models.CharField(max_length=100)
-    nombre_usuario = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=100)
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    nombre_usuario = models.CharField(max_length=60)
+    password = models.CharField(max_length=255)
+    rol = models.ForeignKey(Roles, on_delete=models.CASCADE)
+    activo = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.nombre} {self.apellido} ({self.nombre_usuario})"
