3f960b17f74bc3fb283c5ffd488dac37d4647017
Elias Orihuela <elias.orihuela@gmail.com>
2025-06-03 19:57:27 -0300

    nestor

--- apps/stock/models.py
+++ apps/stock/models.py
@@ -1,15 +1,19 @@
 from django.db import models
+from apps.ingresos.models import Productos
 
 # Create your models here.
-class Stock(models.Model):
-    nombre = models.CharField(max_length=30)
-    precio = models.IntegerField()
-    
-    def __str__(self):
-        return f'{self.nombre} - precio {self.precio}'
-    
-class Deposito(models.Model):
+class Depositos(models.Model):
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
 
     def __str__(self):
-        return f'nombre de deposito: {self.nombre}'
+        return self.nombre
+    
+class StockProductos(models.Model):
+    id = models.AutoField(primary_key=True)
+    producto_id = models.ForeignKey(Productos, on_delete=models.CASCADE)
+    deposito_id = models.ForeignKey(Depositos, on_delete=models.CASCADE)
+    cantidad_total = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.producto} en {self.deposito}: {self.cantidad_total}"
