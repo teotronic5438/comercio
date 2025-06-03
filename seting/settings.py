44a8b880d68bb1d8fb800bcc8bbb7661bd944286
Elias Orihuela <elias.orihuela@gmail.com>
2025-06-03 19:29:32 -0300

    agregando stock

--- seting/settings.py
+++ seting/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.core',  # Asegúrate de que tu aplicación core esté registrada aquí
     'apps.stock',
+    'apps.ordenes'
 ]
 
 MIDDLEWARE = [
