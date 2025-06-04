a61cda8a8092a3d784b77efa4c3ed586281b10c6
Elias Orihuela <elias.orihuela@gmail.com>
2025-06-03 21:21:21 -0300

    agregando modelado ordenes

--- apps/stock/admin.py
+++ apps/stock/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 # traigo la tabla que quiero registrar
-from .models import StockProductos
+from .models import StockProductos, Depositos
 
 # registro la tabla para que me aparezca en el panel
 admin.site.register(StockProductos)
+admin.site.register(Depositos)
