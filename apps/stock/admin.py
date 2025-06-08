262b5686e38d2acd5a3252a0a638c3907760ab1f
Elias Orihuela <elias.orihuela@gmail.com>
2025-06-08 17:13:01 -0300

    agregando configuracion de panel admin

--- apps/stock/admin.py
+++ apps/stock/admin.py
@@ -5,7 +5,16 @@ from django.contrib import admin
 from .models import StockProductos, Depositos
 
 # registro la tabla para que me aparezca en el panel
-admin.site.register(StockProductos)
+# admin.site.register(StockProductos)
+@admin.register(StockProductos)
+class StockProductosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'producto_id', 'deposito_id', 'cantidad_total', 'ubicacion', 'precio_unitario')
+    search_fields = ('producto_id__marca', 'producto_id__modelo', 'deposito_id__nombre')
+    list_filter = ('deposito_id',)
+
+    # def has_add_permission(self, request):
+    #     return False
+
 
 
 # admin.site.register(Depositos)
