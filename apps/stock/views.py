83c554f4fa320939f5fc5031fd03fee5efbb9305
Nestor-Diaz1 <ramiro01032003@gmail.com>
2025-06-17 20:52:33 -0300

    vista de stock pasada a clase

--- apps/stock/views.py
+++ apps/stock/views.py
@@ -1,29 +1,61 @@
-from django.shortcuts import render,HttpResponse
+# from django.shortcuts import render,HttpResponse
+# from .models import StockProductos
+
+# def stock(request):
+#     productos = StockProductos.objects.select_related('producto_id', 'deposito_id').all()
+
+#     busqueda = request.GET.get('busqueda')
+#     filtro = request.GET.get('filtro')
+
+#     if busqueda and filtro:
+#         if filtro == "marca":
+#             productos = productos.filter(producto_id__marca__icontains=busqueda)
+#         elif filtro == "modelo":
+#             productos = productos.filter(producto_id__modelo__icontains=busqueda)
+#         elif filtro == "deposito":
+#             productos = productos.filter(deposito_id__nombre__icontains=busqueda)
+#         elif filtro == "ubicacion":
+#             productos = productos.filter(ubicacion__icontains=busqueda)
+#         elif filtro == "cantidad":
+#             try:
+#                 cantidad = int(busqueda)
+#                 productos = productos.filter(cantidad_total=cantidad)
+#             except ValueError:
+#                 productos = productos.none() 
+
+#     return render(request, 'stock.html', {
+#         'productos': productos,
+#         'show_navbar': True
+#     })
+
+from django.shortcuts import render, HttpResponse
+from django.views import View
 from .models import StockProductos
 
-def stock(request):
-    productos = StockProductos.objects.select_related('producto_id', 'deposito_id').all()
+class StockView(View):
+    def get(self, request):
+        productos = StockProductos.objects.select_related('producto_id', 'deposito_id').all()
 
-    busqueda = request.GET.get('busqueda')
-    filtro = request.GET.get('filtro')
+        busqueda = request.GET.get('busqueda')
+        filtro = request.GET.get('filtro')
 
-    if busqueda and filtro:
-        if filtro == "marca":
-            productos = productos.filter(producto_id__marca__icontains=busqueda)
-        elif filtro == "modelo":
-            productos = productos.filter(producto_id__modelo__icontains=busqueda)
-        elif filtro == "deposito":
-            productos = productos.filter(deposito_id__nombre__icontains=busqueda)
-        elif filtro == "ubicacion":
-            productos = productos.filter(ubicacion__icontains=busqueda)
-        elif filtro == "cantidad":
-            try:
-                cantidad = int(busqueda)
-                productos = productos.filter(cantidad_total=cantidad)
-            except ValueError:
-                productos = productos.none() 
+        if busqueda and filtro:
+            if filtro == "marca":
+                productos = productos.filter(producto_id__marca__icontains=busqueda)
+            elif filtro == "modelo":
+                productos = productos.filter(producto_id__modelo__icontains=busqueda)
+            elif filtro == "deposito":
+                productos = productos.filter(deposito_id__nombre__icontains=busqueda)
+            elif filtro == "ubicacion":
+                productos = productos.filter(ubicacion__icontains=busqueda)
+            elif filtro == "cantidad":
+                try:
+                    cantidad = int(busqueda)
+                    productos = productos.filter(cantidad_total=cantidad)
+                except ValueError:
+                    productos = productos.none()
 
-    return render(request, 'stock.html', {
-        'productos': productos,
-        'show_navbar': True
-    })
+        return render(request, 'stock.html', {
+            'productos': productos,
+            'show_navbar': True
+        })
