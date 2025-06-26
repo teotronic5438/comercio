bfa39759e6a97bd4bb044eb88f5aa1acd76e148c
Elias Orihuela <elias.orihuela@gmail.com>
2025-06-26 17:27:38 -0300

    agregando vista protegida a las rutas de ordenes

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -7,8 +7,9 @@ from .views import dashboard,  home, DashboardOrdenesView
 app_name = 'core'
 urlpatterns = [
    # path('', login, name='Login'),
-    path('dashboard/', dashboard, name='Dashboard'),
+    # path('dashboard/', dashboard, name='Dashboard'),
     #path('logout/', logout, name='Logout'), BORRADO xq se accede desde usuarios:logout
-    path('', home, name ='home'),
-    path('dashboard-ordenes/', DashboardOrdenesView.as_view(), name='dashboard_ordenes'),
+    # path('', home, name ='home'),
+    # path('dashboard-ordenes/', DashboardOrdenesView.as_view(), name='dashboard_ordenes'),
+    path('dashboard/', DashboardOrdenesView.as_view(), name='dashboard_ordenes'),
 ]
