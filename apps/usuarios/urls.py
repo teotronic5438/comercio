32e37a1a99cf58e8870c12d5ac5c28b2efef66eb
Matias Otero <otero.matias.1987@gmail.com>
2025-06-26 01:09:09 -0300

    Cambio de logout basado en funcion a vista

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
-from django.contrib.auth.decorators import login_required
-from .views import  logoutUsuario,  RegistrarUsuario, login_view #, Login
+#from django.contrib.auth.decorators import login_required
+from .views import  LogoutUsuarioView,  RegistrarUsuario, login_view #, Login
 
 
 app_name = 'usuarios'
@@ -9,7 +9,9 @@ urlpatterns = [
     path('register/',  RegistrarUsuario.as_view(), name ='register'),
     #path('accounts/login/',  Login.as_view(), name ='login'), #si usamos Login como clase template 
     path('login/',  login_view, name ='login'), 
-    path('logout/',  login_required(logoutUsuario) , name ='logout'),
+    #path('logout/',  login_required(logoutUsuario) , name ='logout'),
+    path('logout/', LogoutUsuarioView.as_view(), name='logout'),
+    
     
 
 ]
