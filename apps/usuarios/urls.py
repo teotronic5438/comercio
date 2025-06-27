e7421bd37bbc603752d81a7ee578c191f84a7580
Matias Otero <otero.matias.1987@gmail.com>
2025-06-27 12:58:33 -0300

    login como clase

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
-from django.contrib.auth.decorators import login_required
-from .views import  LogoutUsuarioView,  RegistrarUsuario, login_view
+#from django.contrib.auth.decorators import login_required
+from .views import  LogoutUsuarioView, UsuarioLoginView, RegistrarUsuario#, login_view
 # from .views import  logoutUsuario,  RegistrarUsuario, login_view #, Login
 
 
@@ -8,8 +8,8 @@ app_name = 'usuarios'
 urlpatterns = [
  
     path('register/',  RegistrarUsuario.as_view(), name ='register'),
-    #path('accounts/login/',  Login.as_view(), name ='login'), #si usamos Login como clase template 
-    path('login/',  login_view, name ='login'), 
+    #path('login/',  login_view, name ='login'), 
+    path('login/', UsuarioLoginView.as_view(), name='login'),
     path('logout/', LogoutUsuarioView.as_view(), name='logout'),
     # path('logout/',  login_required(logoutUsuario) , name ='logout'),
     
